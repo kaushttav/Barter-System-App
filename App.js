19289642995a7d5e6a32172e44c846c5b775add7
kaushttav <59498306+kaushttav@users.noreply.github.com>
2021-01-02 20:39:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignUpLoginScreen from './screens/SignUpLoginScreen';
+
+export default function App() {
+  return (
+      <View>
+           <SignUpLoginScreen/>   
+      </View>
+  );
+}
